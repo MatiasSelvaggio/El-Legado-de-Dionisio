3d918cae0d47aef0a6887d426cb0808b9e0126dc
MatiasSelvaggio <matias6selvaggio@gmail.com>
2025-02-19 02:37:50 -0300

    test actions git

--- src/test/java/com/api/core/MyTest.java
+++ src/test/java/com/api/core/MyTest.java
@@ -1,9 +1,13 @@
 package com.api.core;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
+@ActiveProfiles("test")
 public class MyTest {
 
     @Test
